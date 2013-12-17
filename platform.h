4bc4595945e2cfe3e9037bc059c7f089f56a875d
michal <michal>
2013-12-17 00:34:15 +0000

    new files for windows

---
+++ platform.h
@@ -0,0 +1,20 @@
+#ifdef WIN
+
+#define PATH_SEP '\\'
+#define DPATH_SEP "\\"
+#define HOMEP "HOMEPATH"
+#define SYS_PROG_PATH "C:\\Xnmr\\prog\\"
+#define COPY_COMM "copy %s %s"
+#define ICON_PATH "C:\\Xnmr\\xnmr_buff_icon.png"
+
+#else
+
+#define PATH_SEP '/'
+#define DPATH_SEP "/"
+#define HOMEP "HOME"
+#define SYS_PROG_PATH "/usr/share/Xnmr/prog/"
+#define COPY_COMM "cp -p %s %s"
+#define ICON_PATH "/usr/share/Xnmr/xnmr_buff_icon.png"
+
+#endif
+
